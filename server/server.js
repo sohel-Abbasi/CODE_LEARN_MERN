5b91ae8f8dc0adaa2c2a0310c100ef1ddbd3ee27
sohel-Abbasi <khansohail7963@gmail.com>
2025-04-24 18:14:12 +0530

    updated server.js

--- server/server.js
+++ server/server.js
@@ -17,10 +17,8 @@ const MONGO_URL = process.env.MONGO_URL;
 const CLIENT_URL = "http://localhost:5173";
 app.use(
   cors({
-    origin: [
+    origin: 
       process.env.CLIENT_URL || CLIENT_URL,
-      "https://code-learn-mern.onrender.com",
-    ],
     methods: ["GET", "POST", "PUT", "DELETE"],
     allowedHeaders: ["Content-Type", "Authorization"],
   })
